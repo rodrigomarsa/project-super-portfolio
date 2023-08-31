783ed28f4f1996264f2675282fc6f88618d60b52
Rodrigo <rodrigomartins.agro@gmail.com>
2023-08-31 18:29:02 -0300

    feat: add project CRUD

--- projects/models.py
+++ projects/models.py
@@ -9,3 +9,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    description = models.TextField(blank=False, max_length=500)
+    github_url = models.URLField(blank=False, max_length=500)
+    keyword = models.CharField(max_length=50, blank=False)
+    key_skill = models.CharField(max_length=50, blank=False)
+    profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="projects"
+    )
+
+    def __str__(self):
+        return f"{self.name}"
--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import ProfileViewSet
+from .views import ProfileViewSet, ProjectViewSet
 
 router = routers.DefaultRouter()
 router.register(r"profiles", ProfileViewSet)
+router.register(r"projects", ProjectViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
